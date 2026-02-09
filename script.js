245ef04ef45a23904f2a9a59b9e17e463418e20b
Khomurod <46620174+Khomurod@users.noreply.github.com>
2026-02-09 10:22:34 -0600

    change

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-let questions = [];
+const PANTRY_ID = "42f7bc17-4c7d-4314-9a0d-19f876d39db6";
+const QUESTIONS_URL = `https://getpantry.cloud/apiv1/pantry/${PANTRY_ID}/basket/questions`;
 
-function addQuestion() {
+async function loadQuestions() {
+  const res = await fetch(QUESTIONS_URL);
+  const data = await res.json();
+  render(data.questions || []);
+}
+
+async function addQuestion() {
   const input = document.getElementById("question");
   if (!input.value) return;
 
+  const res = await fetch(QUESTIONS_URL);
+  const data = await res.json();
+
+  const questions = data.questions || [];
   questions.push(input.value);
+
+  await fetch(QUESTIONS_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ questions })
+  });
+
   input.value = "";
-  render();
+  loadQuestions();
 }
 
-function render() {
+function render(questions) {
   const list = document.getElementById("questions");
   list.innerHTML = "";
-
   questions.forEach(q => {
     const li = document.createElement("li");
     li.innerText = q;
     list.appendChild(li);
   });
 }
+
+loadQuestions();
